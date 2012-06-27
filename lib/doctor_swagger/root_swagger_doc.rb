527b0406ca7d139b096ad45ab5fba25dcff3c144
Michael Xavier <michael@michaelxavier.net>
2012-06-27 10:29:31 -0700

    Handle nil case

--- lib/doctor_swagger/root_swagger_doc.rb
+++ lib/doctor_swagger/root_swagger_doc.rb
@@ -4,7 +4,9 @@ module DoctorSwagger
     def initialize(base_path_extension, options = {}, &block)
       @swagger_version = options.fetch(:swagger_version, DoctorSwagger.swagger_version)
       @api_version     = options.fetch(:api_version, DoctorSwagger.api_version)
-      @base_path       = (options.fetch(:base_path, DoctorSwagger.base_path)) + base_path_extension
+      @base_path       = (options.fetch(:base_path, DoctorSwagger.base_path))
+      @base_path ||= ""
+      @base_path += base_path_extension
       @endpoints       = []
       instance_eval(&block)
     end
